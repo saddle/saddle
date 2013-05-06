9dae8f1e62bc57f8c35f052fb09b9c81f95f5076
Adam Klein <adamklein@gmail.com>
2013-05-06 14:31:35 -0400

    Added crush results

--- saddle-core/src/main/scala/org/saddle/util/RandomGen.scala
+++ saddle-core/src/main/scala/org/saddle/util/RandomGen.scala
@@ -54,6 +54,51 @@ import java.io.BufferedInputStream
  *  ----------------------------------------------
  *  All other tests were passed
  *
+ * ========= Summary results of Crush =========
+ *  Version:          TestU01 1.2.3
+ *  Generator:        STDIN
+ *  Number of statistics:  144
+ *  Total CPU time:   00:57:39.11
+ *  The following tests gave p-values outside [0.001, 0.9990]:
+ *  (eps  means a value < 1.0e-300):
+ *  (eps1 means a value < 1.0e-15):
+ *
+ *        Test                          p-value
+ *  ----------------------------------------------
+ *   3  CollisionOver, t = 2           1.4e-10
+ *   6  CollisionOver, t = 4             eps
+ *  10  CollisionOver, t = 20          1.3e-62
+ *  11  BirthdaySpacings, t = 2         5.2e-5
+ *  16  BirthdaySpacings, t = 8        1.7e-12
+ *  17  BirthdaySpacings, t = 8        3.3e-73
+ *  34  Gap, r = 22                      eps
+ *  56  MatrixRank, 60 x 60              eps
+ *  57  MatrixRank, 60 x 60              eps
+ *  58  MatrixRank, 300 x 300            eps
+ *  59  MatrixRank, 300 x 300            eps
+ *  60  MatrixRank, 1200 x 1200          eps
+ *  61  MatrixRank, 1200 x 1200          eps
+ *  65  RandomWalk1 H (L = 90)           eps
+ *  65  RandomWalk1 M (L = 90)           eps
+ *  66  RandomWalk1 H (L = 90)           eps
+ *  66  RandomWalk1 M (L = 90)           eps
+ *  66  RandomWalk1 J (L = 90)           eps
+ *  67  RandomWalk1 H (L = 1000)         eps
+ *  67  RandomWalk1 M (L = 1000)        2.0e-9
+ *  67  RandomWalk1 C (L = 1000)        1.0e-4
+ *  68  RandomWalk1 H (L = 1000)       2.0e-12
+ *  68  RandomWalk1 M (L = 1000)         eps
+ *  68  RandomWalk1 J (L = 1000)         eps
+ *  69  RandomWalk1 C (L = 10000)       9.7e-4
+ *  71  LinearComp, r = 0              1 - eps1
+ *  72  LinearComp, r = 29             1 - eps1
+ *  85  HammingIndep, L = 30             eps
+ *  86  HammingIndep, L = 30             eps
+ *  87  HammingIndep, L = 300            eps
+ *  88  HammingIndep, L = 300            eps
+ *  ----------------------------------------------
+ *  All other tests were passed
+ *
  * ========= Summary results of BigCrush =========
  *  Version:          TestU01 1.2.3
  *  Generator:        STDIN
@@ -110,6 +155,15 @@ object Xor64 {
  *  ----------------------------------------------
  *  All other tests were passed
  *
+ *
+ * ========= Summary results of Crush =========
+ *  Version:          TestU01 1.2.3
+ *  Generator:        STDIN
+ *  Number of statistics:  144
+ *  Total CPU time:   01:00:08.48
+ *
+ *  All tests were passed
+ *
  * ========= Summary results of BigCrush =========
  *  Version:          TestU01 1.2.3
  *  Generator:        STDIN
@@ -131,6 +185,14 @@ object LFib {
  *  Generator:             STDIN
  *  Number of statistics:  15
  *  Total CPU time:        00:00:18.85
+ *
+ *  All tests were passed
+ *
+ * ========= Summary results of Crush =========
+ *  Version:          TestU01 1.2.3
+ *  Generator:        STDIN
+ *  Number of statistics:  144
+ *  Total CPU time:   01:00:08.48
  *
  *  All tests were passed
  *
