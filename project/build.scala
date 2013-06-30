d1aaa9279e5e8c2997f0581bcf9f4b68f72d3021
Adam Klein <adamklein@gmail.com>
2013-06-30 18:46:34 -0400

    Version update

--- project/build.scala
+++ project/build.scala
@@ -129,7 +129,7 @@ object Shared {
         </developer>
       </developers>
     ),
-    version := "1.3.1",
+    version := "1.4.0-SNAPSHOT",
     scalaVersion := "2.9.2",
     crossScalaVersions := Seq("2.9.2", "2.9.3", "2.10.0"),
     scalacOptions := Seq("-deprecation", "-unchecked"), // , "-Xexperimental"),
