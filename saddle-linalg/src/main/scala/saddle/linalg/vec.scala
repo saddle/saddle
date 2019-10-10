f32ec8d8447eddbb951684540658072b0fe944e8
Istvan Bartha <bartha.pityu@gmail.com>
2019-10-10 11:16:18 +0200

    Add ScalaDoc to VecLinalgOps

--- saddle-linalg/src/main/scala/saddle/linalg/vec.scala
+++ saddle-linalg/src/main/scala/saddle/linalg/vec.scala
@@ -20,6 +20,7 @@ trait VecLinalgOps {
       implicit op: VecBinOp[Vec[Double], Double]
   ): Double = op(self, other)
 
+  /* Sum. Does not filter out NAs*/
   def sum2 = {
     var s = 0d
     var i = 0
@@ -32,6 +33,7 @@ trait VecLinalgOps {
     s
   }
 
+  /* One pass mean using Welford's algorithm. Does not filter out NAs */
   def mean2 = {
     val n = self.length
     val ar = self.toArray
@@ -45,6 +47,7 @@ trait VecLinalgOps {
     xm
   }
 
+  /* One pass sample variance using Welford's algorithm. Does not filter out NAs */
   def sampleVariance = {
     val n = self.length
     val ar = self.toArray
@@ -61,11 +64,12 @@ trait VecLinalgOps {
     m / (n - 1)
   }
 
+  /* Subtracts the mean from each element. Does not filter out NAs */
   def demeaned = {
     val mean = this.mean2
     val ar1 = self.toArray
     val n = ar1.length
-    val ar2 = Array.ofDim[Double](n )
+    val ar2 = Array.ofDim[Double](n)
     var i = 0
     while (i < n) {
       ar2(i) = ar1(i) - mean
@@ -74,7 +78,9 @@ trait VecLinalgOps {
     Vec(ar2)
   }
 
-  /* https://prod-ng.sandia.gov/techlib-noauth/access-control.cgi/2008/086212.pdf */
+  /** One pass Pearson correlation coefficient. Does not filter out NAs.
+    * https://prod-ng.sandia.gov/techlib-noauth/access-control.cgi/2008/086212.pdf
+    */
   def pearson(other: Vec[Double]) = {
     val n = self.length
     assert(n == other.length)
@@ -101,6 +107,10 @@ trait VecLinalgOps {
     covS / math.sqrt(varS1 * varS2)
   }
 
+  /** One pass covariance of two vectors
+    *
+    * https://prod-ng.sandia.gov/techlib-noauth/access-control.cgi/2008/086212.pdf
+    */
   def sampleCovariance(other: Vec[Double]) = {
     val n = self.length
     assert(n == other.length)
